2fedaecd541f6f61e15780d9b110a7fad6439409
nicolalandro <nicolaxx94@live.it>
2021-01-28 15:54:07 +0100

    new experiments

--- log_analisys/folder_plot.py
+++ log_analisys/folder_plot.py
@@ -13,7 +13,16 @@ def main():
     f, (ax, axleg) = plt.subplots(2, figsize=(10, 10))
 
     files = os.listdir(folder_path)
-    files = list(filter(lambda x: not x.startswith('mas_'), files))
+    # files = list(filter(lambda x: not x.startswith('mas_'), files))
+    files = list(filter(lambda x: (x.startswith('sgdm_') and x.endswith('_1.log'))
+                                  or not x.startswith('sgdm_'),
+                        files))
+    files = list(filter(lambda x: (x.startswith('padam_lr_0.01_') and x.endswith('_1.log'))
+                                  or not x.startswith('padam_'),
+                        files))
+    files = list(filter(lambda x: not (x.startswith('mas') or x.startswith('mps') or x.startswith('map')),
+                        files))
+
     files.sort()
     for f in files:
         log_path = os.path.join(folder_path, f)
